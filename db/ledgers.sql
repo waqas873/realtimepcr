b9b4a6085a871af560220f756857a6f8fced3f1c
waqas873 <muhammadw873@gmail.com>
2021-05-30 02:14:00 +0500

    work

--- db/ledgers.sql
+++ db/ledgers.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 27, 2021 at 08:28 PM
+-- Generation Time: May 29, 2021 at 09:37 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.2
 
@@ -36,6 +36,7 @@ CREATE TABLE `ledgers` (
   `lab_id` int(11) DEFAULT NULL,
   `doctor_id` int(11) DEFAULT NULL,
   `embassy_user_id` int(11) DEFAULT NULL,
+  `airline_user_id` int(11) DEFAULT NULL,
   `system_invoice_id` int(11) DEFAULT NULL,
   `unique_id` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `description` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -50,18 +51,20 @@ CREATE TABLE `ledgers` (
 -- Dumping data for table `ledgers`
 --
 
-INSERT INTO `ledgers` (`id`, `user_id`, `invoice_id`, `collection_point_id`, `lab_id`, `doctor_id`, `embassy_user_id`, `system_invoice_id`, `unique_id`, `description`, `amount`, `is_debit`, `is_credit`, `created_at`, `updated_at`) VALUES
-(1, 8, NULL, 1, NULL, NULL, NULL, NULL, '410774', 'Amount received from patient', 7500, 1, 0, '2021-05-10 20:52:46', '2021-05-10 20:52:46'),
-(2, 8, NULL, 1, NULL, NULL, NULL, NULL, '273269', 'Amount received from patient', 1000, 0, 1, '2021-05-10 20:54:26', '2021-05-10 20:54:26'),
-(5, 8, NULL, 1, NULL, NULL, NULL, NULL, '66536', 'Amount received from patient', 1200, 1, 0, '2021-05-10 21:06:57', '2021-05-10 21:06:57'),
-(6, 13, NULL, 1, NULL, NULL, NULL, 8, '643997', 'Amount recieved from collection point.', 3500, 0, 1, '2021-05-17 20:04:04', '2021-05-18 20:03:51'),
-(8, 13, NULL, 1, NULL, NULL, NULL, 10, '570982', 'Amount recieved from collection point.', 1500, 0, 1, '2021-05-18 19:29:07', '2021-05-18 19:29:07'),
-(10, 13, NULL, 1, NULL, NULL, NULL, 12, '760272', 'Amount recieved from collection point.', 1200, 0, 1, '2021-05-18 20:32:02', '2021-05-18 20:32:02'),
-(11, 8, 1485, 1, NULL, NULL, NULL, NULL, '40094', 'Amount received from patient', 5200, 1, 0, '2021-05-21 19:51:45', '2021-05-21 19:51:45'),
-(12, 8, 1486, 1, NULL, NULL, NULL, NULL, '191902', 'Amount received from patient', 5200, 1, 0, '2021-05-21 20:22:33', '2021-05-21 20:22:33'),
-(13, 8, 1487, 1, NULL, NULL, NULL, NULL, '497337', 'Amount received from patient', 4800, 1, 0, '2021-05-21 20:25:25', '2021-05-21 20:25:25'),
-(15, 8, 1491, 1, NULL, NULL, NULL, NULL, '525763', 'Amount received from patient', 4800, 1, 0, '2021-05-21 20:31:31', '2021-05-21 20:31:31'),
-(16, 8, 1491, NULL, NULL, 2, NULL, NULL, '356511', 'Doctors Commssion', 2400, 1, 0, '2021-05-21 20:31:31', '2021-05-21 20:31:31');
+INSERT INTO `ledgers` (`id`, `user_id`, `invoice_id`, `collection_point_id`, `lab_id`, `doctor_id`, `embassy_user_id`, `airline_user_id`, `system_invoice_id`, `unique_id`, `description`, `amount`, `is_debit`, `is_credit`, `created_at`, `updated_at`) VALUES
+(1, 8, NULL, 1, NULL, NULL, NULL, NULL, NULL, '410774', 'Amount received from patient', 7500, 1, 0, '2021-05-10 20:52:46', '2021-05-10 20:52:46'),
+(2, 8, NULL, 1, NULL, NULL, NULL, NULL, NULL, '273269', 'Amount received from patient', 1000, 0, 1, '2021-05-10 20:54:26', '2021-05-10 20:54:26'),
+(5, 8, NULL, 1, NULL, NULL, NULL, NULL, NULL, '66536', 'Amount received from patient', 1200, 1, 0, '2021-05-10 21:06:57', '2021-05-10 21:06:57'),
+(6, 13, NULL, 1, NULL, NULL, NULL, NULL, 8, '643997', 'Amount recieved from collection point.', 3500, 0, 1, '2021-05-17 20:04:04', '2021-05-18 20:03:51'),
+(8, 13, NULL, 1, NULL, NULL, NULL, NULL, 10, '570982', 'Amount recieved from collection point.', 1500, 0, 1, '2021-05-18 19:29:07', '2021-05-18 19:29:07'),
+(10, 13, NULL, 1, NULL, NULL, NULL, NULL, 12, '760272', 'Amount recieved from collection point.', 1200, 0, 1, '2021-05-18 20:32:02', '2021-05-18 20:32:02'),
+(11, 8, 1485, 1, NULL, NULL, NULL, NULL, NULL, '40094', 'Amount received from patient', 5200, 1, 0, '2021-05-21 19:51:45', '2021-05-21 19:51:45'),
+(12, 8, 1486, 1, NULL, NULL, NULL, NULL, NULL, '191902', 'Amount received from patient', 5200, 1, 0, '2021-05-21 20:22:33', '2021-05-21 20:22:33'),
+(13, 8, 1487, 1, NULL, NULL, NULL, NULL, NULL, '497337', 'Amount received from patient', 4800, 1, 0, '2021-05-21 20:25:25', '2021-05-21 20:25:25'),
+(15, 8, 1491, 1, NULL, NULL, NULL, NULL, NULL, '525763', 'Amount received from patient', 4800, 1, 0, '2021-05-21 20:31:31', '2021-05-21 20:31:31'),
+(16, 8, 1491, NULL, NULL, 2, NULL, NULL, NULL, '356511', 'Doctors Commssion', 2400, 1, 0, '2021-05-21 20:31:31', '2021-05-21 20:31:31'),
+(19, 1, 1494, NULL, NULL, NULL, 30, NULL, NULL, '724168', 'Embassy Commssion', 200, 1, 0, '2021-05-29 19:08:11', '2021-05-29 19:08:11'),
+(20, 13, NULL, NULL, NULL, NULL, 30, NULL, 15, '227566', 'Commission delivered to embassy.', 50, 0, 1, '2021-05-29 19:10:20', '2021-05-29 19:10:20');
 
 --
 -- Indexes for dumped tables
@@ -81,7 +84,7 @@ ALTER TABLE `ledgers`
 -- AUTO_INCREMENT for table `ledgers`
 --
 ALTER TABLE `ledgers`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- db/system_invoices.sql
+++ db/system_invoices.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 27, 2021 at 10:50 PM
+-- Generation Time: May 29, 2021 at 09:37 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.2
 
@@ -34,6 +34,7 @@ CREATE TABLE `system_invoices` (
   `collection_point_id` int(11) DEFAULT NULL,
   `doctor_id` int(11) DEFAULT NULL,
   `embassy_user_id` int(11) DEFAULT NULL,
+  `airline_user_id` int(11) DEFAULT NULL,
   `unique_id` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `amount` int(11) DEFAULT NULL,
   `payment_method` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -48,11 +49,12 @@ CREATE TABLE `system_invoices` (
 -- Dumping data for table `system_invoices`
 --
 
-INSERT INTO `system_invoices` (`id`, `user_id`, `collection_point_id`, `doctor_id`, `embassy_user_id`, `unique_id`, `amount`, `payment_method`, `description`, `is_recieved`, `date`, `created_at`, `updated_at`) VALUES
-(7, 13, 1, NULL, NULL, '191106', 567, 'Bank Transfer', NULL, 1, '2021-05-18', '2021-05-17 20:01:47', '2021-05-17 20:01:47'),
-(8, 13, 1, NULL, NULL, '994529', 3500, 'Cash', 'testing-894723429423', 1, '2021-05-18', '2021-05-17 20:04:04', '2021-05-18 20:03:51'),
-(10, 13, 1, NULL, NULL, '134325', 1500, 'Cash', 'teesti', 1, '2021-05-19', '2021-05-18 19:29:07', '2021-05-18 19:29:07'),
-(12, 13, 1, NULL, NULL, '794791', 1200, 'Payment Gateway', 'hello world', 1, '2021-05-19', '2021-05-18 20:32:02', '2021-05-18 20:32:02');
+INSERT INTO `system_invoices` (`id`, `user_id`, `collection_point_id`, `doctor_id`, `embassy_user_id`, `airline_user_id`, `unique_id`, `amount`, `payment_method`, `description`, `is_recieved`, `date`, `created_at`, `updated_at`) VALUES
+(7, 13, 1, NULL, NULL, NULL, '191106', 567, 'Bank Transfer', NULL, 1, '2021-05-18', '2021-05-17 20:01:47', '2021-05-17 20:01:47'),
+(8, 13, 1, NULL, NULL, NULL, '994529', 3500, 'Cash', 'testing-894723429423', 1, '2021-05-18', '2021-05-17 20:04:04', '2021-05-18 20:03:51'),
+(10, 13, 1, NULL, NULL, NULL, '134325', 1500, 'Cash', 'teesti', 1, '2021-05-19', '2021-05-18 19:29:07', '2021-05-18 19:29:07'),
+(12, 13, 1, NULL, NULL, NULL, '794791', 1200, 'Payment Gateway', 'hello world', 1, '2021-05-19', '2021-05-18 20:32:02', '2021-05-18 20:32:02'),
+(15, 13, NULL, NULL, 30, NULL, '6636', 50, 'Bank Transfer', NULL, 1, '2021-05-20', '2021-05-29 19:10:20', '2021-05-29 19:10:20');
 
 --
 -- Indexes for dumped tables
@@ -72,7 +74,7 @@ ALTER TABLE `system_invoices`
 -- AUTO_INCREMENT for table `system_invoices`
 --
 ALTER TABLE `system_invoices`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
